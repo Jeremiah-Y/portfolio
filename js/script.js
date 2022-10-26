0fa13eb8051670f5f43e1f53a53add48a5abeb20
Jeremiah <jeremiah.y@hotmail.com>
2022-10-26 23:39:47 +0200

    add collapsible

--- js/script.js
+++ js/script.js
@@ -31,56 +31,21 @@ mybutton.addEventListener('click', () => {
 
 
 
+//collapsible content
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-
-// Get the button:
-// let mybutton = document.getElementById("float-button");
-
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function() {scrollFunction()};
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0; // For Safari
-//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 
 
 
-
-//try lazy loading
-// document.addEventListener("DOMContentLoaded", function() {
-//   var lazyloadImages = document.querySelectorAll("img.lazy");    
-//   var lazyloadThrottleTimeout;
-  
-//   function lazyload () {
-//     if(lazyloadThrottleTimeout) {
-//       clearTimeout(lazyloadThrottleTimeout);
-//     }    
-    
-//     lazyloadThrottleTimeout = setTimeout(function() {
-//         var scrollTop = window.pageYOffset;
-//         lazyloadImages.forEach(function(img) {
-//             if(img.offsetTop < (window.innerHeight + scrollTop)) {
-//               img.src = img.dataset.src;
-//               img.classList.remove('lazy');
-//             }
-//         });
-//         if(lazyloadImages.length == 0) { 
-//           document.removeEventListener("scroll", lazyload);
-//           window.removeEventListener("resize", lazyload);
-//           window.removeEventListener("orientationChange", lazyload);
-//         }
-//     }, 20);
-//   }
-  
-//   document.addEventListener("scroll", lazyload);
-//   window.addEventListener("resize", lazyload);
-//   window.addEventListener("orientationChange", lazyload);
-// });
