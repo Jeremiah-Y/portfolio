1ba254bdd45a7e2581073c5689878e225923877b
Jeremiah <jeremiah.y@hotmail.com>
2022-10-27 18:32:17 +0200

    add dropdown

--- js/script.js
+++ js/script.js
@@ -39,10 +39,10 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
+    if (content.style.display === "inline-block") {
       content.style.display = "none";
     } else {
-      content.style.display = "block";
+      content.style.display = "inline-block";
     }
   });
 }
